8184280f08beca8d627d840b82f9c6167ba3dcb4
raso85 <mrdja85@gmail.com>
2014-02-20 20:12:37 +0100

    initial

---
+++ TreeToGrid/app/controller/Startup.js
@@ -0,0 +1,16 @@
+Ext.define('TreeToGrid.controller.Startup', {
+    extend: 'Ext.app.Controller',
+    views: ['Viewport','MainPanel','LeftPanel'],
+    refs: [
+        {ref: 'viewport', selector: 'viewport'},
+        {ref: 'mainPanel', selector: 'mainPanel'},
+        {ref:'leftPanel', selector:'leftPanel'} 
+    ],
+    init: function(){
+        this.control({
+            
+        });
+    }
+    
+
+})
---
+++ TreeToGrid/app/store/TreeData.js
@@ -0,0 +1,14 @@
+Ext.define('TreeToGrid.store.TreeData', {
+    extend: 'Ext.data.TreeStore',
+    root: {
+        expanded: true,
+        children: [
+            { text: "detention", leaf: true, qtitle:{price:527} },
+            { text: "homework", expanded: true,qtitle:{price:8589}, children: [
+                { text: "book report", leaf: true, qtitle:{price:7852} },
+                { text: "algebra", leaf: true, qtitle:{price:4747}}
+            ] },
+            { text: "buy lottery tickets", leaf: true , qtitle:{price:63875}}
+        ]
+    }
+});
